42c5acae93cebb9324cd17f16e5221cd5a63b573
Julienqsl <129181500+Julienqsl@users.noreply.github.com>
2023-10-10 19:50:08 +0200

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,17 +1,29 @@
 const UnauthorizedError = require("../errors/unauthorized");
 const jwt = require("jsonwebtoken");
 const config = require("../config");
+const usersService = require("../path/to/users.service"); // Remplacez le chemin par le chemin correct vers votre service utilisateur
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   try {
     const token = req.headers["x-access-token"];
     if (!token) {
-      throw "not token";
+      throw "No token provided";
     }
+    
     const decoded = jwt.verify(token, config.secretJwtToken);
-    req.user = decoded;
+    
+    // Récupérez toutes les informations de l'utilisateur en fonction de l'ID
+    const user = await usersService.get(decoded.userId);
+    
+    if (!user) {
+      throw "User not found";
+    }
+
+    // Attachez toutes les informations de l'utilisateur à l'objet req.user
+    req.user = user;
+
     next();
-  } catch (message) {
-    next(new UnauthorizedError(message));
+  } catch (error) {
+    next(new UnauthorizedError(error.message || "Unauthorized"));
   }
 };
